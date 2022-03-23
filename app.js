fbcbd300a992db146d8916d493555a6cbccf1bef
mattand <mjande@umich.edu>
2022-03-22 20:36:44 -0400

    Add most functionalities and UI

--- app.js
+++ app.js
@@ -1,28 +1,109 @@
+// math functions
+function round(x) {
+    return Math.round(10 * x) / 10;
+}
+
 function add(a, b) {
-    return a + b; 
+    a = parseFloat(a);
+    b = parseFloat(b);
+    return round(a + b); 
 }   
 
 function subtract(a, b) {
-    return a - b;
+    return round(a - b);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return round(a * b);
 }
 
 function divide(a, b) {
-    return a / b;
+    if (b == undefined) {
+        display("Good luck with that.")
+        reset = true;
+    } else {
+    return round(a / b);
+    }
 }
 
 function operate(op, a, b) {
     switch (op) {
-        case 'opAdd':
+        case '+':
             return add(a, b);
-        case 'opSubtract':
+        case '-':
             return subtract(a, b);
-        case 'opMultiply':
+        case '*':
             return multiply(a, b);
-        case 'opDivide':
+        case '/':
             return divide(a, b);
     }
 }
+
+function display(string) {
+    document.querySelector(".display").textContent = string;
+}
+
+function returnDisplay() {
+    return document.querySelector(".display").textContent;
+}
+
+function resetDisplay() {
+    document.querySelector(".display").textContent = undefined;
+}
+
+// Event listeners
+let storedValue; 
+let queuedOp;
+let reset; 
+
+function input(numButton) {
+    if (reset === true) {
+        display(numButton)
+        reset = false;
+    } else {
+        let displayValue = document.querySelector(".display").textContent;
+        displayValue += numButton;
+        display(displayValue);
+    }
+}
+
+let numberButtons = document.querySelectorAll("button.number");
+numberButtons.forEach((numberButton) => {
+    let number = numberButton.textContent;
+    numberButton.addEventListener('click', function() {
+        input(number);
+    });
+});
+
+document.querySelector(".clear").addEventListener('click', () => {
+    storedValue = undefined;
+    queuedOp = undefined;
+    resetDisplay();
+    reset = false;
+});
+
+let operators = document.querySelectorAll('.operator');
+operators.forEach((operator) => { 
+    let operatorValue = operator.textContent;
+    operator.addEventListener('click', () => {    
+        if (storedValue === undefined) {
+            storedValue = returnDisplay();
+            storedOp = operatorValue;
+            resetDisplay();
+        } else {
+            storedValue = operate(storedOp, storedValue, returnDisplay());
+            display(storedValue);
+            storedOp = operatorValue;
+            reset = true;
+        }     
+    });
+});
+
+document.querySelector(".equals").addEventListener('click', () => {
+    let solution = operate(storedOp, storedValue, returnDisplay());
+    console.log(solution);
+    display(solution);
+    storedOp = undefined;
+    storedValue = undefined;
+    reset = true;
+});
